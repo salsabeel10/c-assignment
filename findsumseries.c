a6c1204d6df560bd79f7a3eba797408170798924
salsabeel10 <62097696+salsabeel10@users.noreply.github.com>
2020-03-13 14:54:21 +0530

    Create findsumseries.c

---
+++ findsumseries.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+  {
+   int n,sum,i;
+   printf("enter the value of n:");
+   scanf("%d",&n);
+   
+   for(i=0;i<n;i++)
+       {
+        sum=i*i;
+       }
+    printf("sum=%d",sum);
+  } 
